488bc422052940eeea2e76222e116a769f53419c
OmPrakash109 <om.ssb51@gmail.com>
2024-03-09 16:41:41 +0530

    Done the N Queens Problem

--- src/NQueen.java
+++ src/NQueen.java
@@ -1,41 +1,79 @@
 import java.util.Scanner;
 public class NQueen{
+    public static int count=0;
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
         char[][] board=new char[n][n];
-        for(int i=0;i<n;i++) {
-            for (int j = 0; j < n; j++) {
-                board[i][j] = 'X';
-            }
-        }
         nqueen(board,0);
     }
     public static void nqueen(char[][] board, int row){
-        int n=board.length;
+        int n=board[0].length;
+        if(row==n){
+            count++;
+            System.out.println("possibility "+count+": \n");
+            for(int i=0;i<n;i++) {
+                for (int j = 0; j < n; j++) {
+                    if(board[i][j]=='Q')
+                    System.out.print(" 👑 ");
+                    else
+                        System.out.print(" . ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+            return;
+        }
         for(int j=0;j<n;j++) {
-            if (isSafe(board, row, col)) {
+            if (isSafe(board, row,j)) {
                 board[row][j]='Q';
                 nqueen(board,row+1);
                 board[row][j]='X'; //backtracking
             }
         }
-        if(row==n){
-            for(int i=0;i<n;i++) {
-                for (int j = 0; j < n; j++) {
-                    System.out.print(board[i][j]);
-                }
-                System.out.println();
-            }
-            return;
-        }
+
     }
     public static boolean isSafe(char[][] board, int row, int col){
         int n=board.length;
         for(int j=0;j<n;j++){
             if(board[row][j]=='Q') return false;
         }
-        f
+        for(int i=0;i<n;i++){
+            if(board[i][col]=='Q') return false;
+        }
+        int i=row;
+        int j=col;
+        //north east
+        while(i>=0&&j<n){
+            if(board[i][j]=='Q') return false;
+            i--;
+            j++;
+        }
+        i=row;
+        j=col;
+        //south east
+        while(i<n&&j<n){
+            if(board[i][j]=='Q') return false;
+            i++;
+            j++;
+        }
+        i=row;
+        j=col;
+        //north west
+        while(i>=0&&j>=0){
+            if(board[i][j]=='Q') return false;
+            i--;
+            j--;
+        }
+        i=row;
+        j=col;
+        //south west
+        while(i<n&&j>=0){
+            if(board[i][j]=='Q') return false;
+            i++;
+            j--;
+        }
+        return true;
 
     }
 }
--- src/nQueens.java
+++ src/nQueens.java
@@ -1,65 +1,75 @@
-//import java.util.Scanner;
-// public class nQueens{
-//     public static void main(String[] args){
-//         Scanner sc = new Scanner(System.in);
-//         System.out.println("Enter the number of queens : ");
-//         int n = sc.nextInt();
-//         char[][] board = new char[n][n];
-//         for(int i=0;i<n;i++){
-//             for(int j=0;j<n;j++){
-//                 board[i][j]='.';
-//             }
-//         }
-//         nqueen(board,0);
-//     }
-//     public static void nqueen(char[][] board, int row){
-//         int n=board.length;
-//         if(row==n){
-//             for(int i=0;i<n;i++){
-//                 for(int j=0;j<n;j++){
-//                     System.out.println(board[i][j]);
-//                 }
-//             }
-//             return;
-//         }
-//         for(int j=0;j<n;j++){
-//            if(isSafe(board,row,j)){
-//                board[row][j]='Q';
-//                nqueen(board,row+1);
-//                board[row][j]='X'; //backtracking
-//            }
-//         }
-//
-//     }
-//     public static boolean isSafe(char[][] board, int row, int col){
-//         int n=board.length;
-//         for(int j=0;j<n;j++){
-//             if(board[row][j]=='Q') return false;
-//         }
-//         for(int i=0;i<n;i++){
-//             if(board[i][col]=='Q') return false;
-//         }
-//         while(row>=0 && col<n){
-//             if(board[row][col]=='Q') return false;
-//             row--;
-//             col++;
-//         }
-//         while(row>=0&&col>=0){
-//             if(board[row][col]=='Q') return false;
-//             row--;
-//             col--;
-//         }
-//         while(row<n && col<n){
-//             if(board[row][col]=='Q') return false;
-//             row++;
-//             col++;
-//         }
-//         while(row<n && col>=0){
-//             if(board[row][col]=='Q') return false;
-//             row++;
-//             col--;
-//         }
-//         return true;
-//
-//     }
-// }
+import java.util.Scanner;
+ public class nQueens{
+     public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter the number of queens : ");
+         int n = sc.nextInt();
+         char[][] board = new char[n][n];
+         for(int i=0;i<n;i++){
+             for(int j=0;j<n;j++){
+                 board[i][j]='.';
+             }
+         }
+         nqueen(board,0);
+     }
+     public static void nqueen(char[][] board, int row){
+         int n=board.length;
+         if(row==n){
+             for(int i=0;i<n;i++){
+                 for(int j=0;j<n;j++){
+                     System.out.print(board[i][j]+" ");
+                 }
+                 System.out.println();
+             }
+             System.out.println("\n\n\n\n");
+             return;
+         }
+         for(int j=0;j<n;j++){
+            if(isSafe(board,row,j)){
+                board[row][j]='Q';
+                nqueen(board,row+1);
+                board[row][j]='.'; //backtracking
+            }
+         }
+
+     }
+     public static boolean isSafe(char[][] board, int rower, int column){
+         int n=board.length;
+         for(int j=0;j<n;j++){
+             if(board[rower][j]=='Q') return false;
+         }
+         for(int i=0;i<n;i++){
+             if(board[i][column]=='Q') return false;
+         }
+         int row=rower;
+         int col=column;
+         while(row>=0 && col<n){
+             if(board[row][col]=='Q') return false;
+             row--;
+             col++;
+         }
+         row=rower;
+         col=column;
+         while(row>=0&&col>=0){
+             if(board[row][col]=='Q') return false;
+             row--;
+             col--;
+         }
+         row=rower;
+         col=column;
+         while(row<n && col<n){
+             if(board[row][col]=='Q') return false;
+             row++;
+             col++;
+         }
+         row=rower;
+         col=column;
+         while(row<n && col>=0){
+             if(board[row][col]=='Q') return false;
+             row++;
+             col--;
+         }
+         return true;
+
+     }
+ }
